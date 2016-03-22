dc26d0b7a7bb5097726cd7c81f7b9eea926c2126
cesarwbr <cesarwbr@gmail.com>
2016-03-22 09:24:33 -0300

    day 2

--- app/app.js
+++ app/app.js
@@ -1,1 +1,28 @@
+var angular = require('angular');
+require('angular-route');
 
+var app = angular.module('customersApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'CustomersCtrl',
+        templateUrl: '/app/views/customers.html'
+    })
+    .when('/orders', {
+        controller: 'OrdersCtrl',
+        templateUrl: '/app/views/orders.html'
+    });
+});
+
+app.controller('OrdersCtrl', function($scope) {
+  $scope.customerId = 4;
+});
+
+app.controller('CustomersCtrl', function($scope) {
+  $scope.customers = [
+      {id: 1, name: 'Joao', total: 5.996},
+      {id: 2, name: 'Maria', total: 10.994},
+      {id: 3, name: 'Jose', total: 10.99},
+      {id: 4, name: 'Cesar', total: 15.994},
+  ];
+});
