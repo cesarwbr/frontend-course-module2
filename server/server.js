568402f5b8080e6506a594a981a50697e105a67d
Cesar William <cesarwbr@gmail.com>
2016-03-27 20:37:53 -0300

    Add crossdomain

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ var data = require('./data.js');
 
 express()
     .get('/api/customer', function(req, res) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
         res.json(data.customers);
     })
     .listen(3000, function() {
