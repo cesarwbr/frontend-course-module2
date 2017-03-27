3d915abcea126000dd29839369ae9fd4a6d27d6a
Cesar William <cesarwbr@gmail.com>
2017-03-27 09:08:34 -0300

    Add test

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -1,56 +1,19 @@
-var angular = require('angular');
-require('angular-route');
+import angular from 'angular'
+import 'angular-route'
 
-var app = angular.module('customersApp', ['ngRoute']);
+// local
+import CustomersCtrl from './controllers/customers-ctrl'
+import OrdersCtrl from './controllers/orders-ctrl'
+import CustomersFactory from './services/customers-factory'
+import CustomerCardDirective from './directives/customer-card-directive'
+import CheckmarkFilter from './filters/checkmark-filter'
+import AppConfig from './config'
 
-app.config(function($routeProvider) {
-    $routeProvider.when('/', {
-        controller: 'CustomersCtrl',
-        templateUrl: '/app/views/customers.html'
-    })
-    .when('/orders', {
-        controller: 'OrdersCtrl',
-        templateUrl: '/app/views/orders.html'
-    });
-});
+angular.module('customersApp', ['ngRoute'])
+  .config(AppConfig)
+  .controller('OrdersCtrl', OrdersCtrl)
+  .controller('CustomersCtrl', CustomersCtrl)
+  .factory('CustomersFactory', CustomersFactory)
+  .directive('customerCard', CustomerCardDirective)
+  .filter('checkmark', CheckmarkFilter)
 
-app.controller('OrdersCtrl', function($scope) {
-  $scope.customerId = 4;
-});
-
-app.controller('CustomersCtrl', function($scope, CustomersFactory) {
-  $scope.name = 'test';
-  CustomersFactory.getCustomers().success(function(data) {
-    $scope.customers = data;
-    $scope.customersTotal = data.length;
-  });
-});
-
-app.factory('CustomersFactory', function($http) {
-  var factory = {};
-
-  factory.getCustomers = function() {
-    return $http.get('http://localhost:3000/api/customer');
-  };
-
-  return factory;
-});
-
-app.directive('customerCard', function(CustomersFactory) {
-  return {
-    restrict: 'E',
-    scope: {
-      customer: '=data'
-    },
-    templateUrl: '/app/views/customer.html',
-    replace: true
-  };
-});
-
-app.filter('checkmark', function() {
-  return function(input) {
-    var check = input > 10 ? '\u2713' : '\u2718';
-
-    return input + ' ' + check;
-  };
-});
